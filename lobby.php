75763962445fdcd2d8c145fb891dc766ee1bb37d
Nick <nickspierenburg@hotmail.com>
2018-03-20 13:16:11 +0100

    Lobby layout

--- lobby.php
+++ lobby.php
@@ -38,8 +38,12 @@ if(!isset($_SESSION['user'])) {
   <link rel="stylesheet" type="text/css" href="https://semantic-ui.com/dist/components/icon.css">
   <link rel="stylesheet" type="text/css" href="https://semantic-ui.com/dist/components/sidebar.css">
   <link rel="stylesheet" type="text/css" href="https://semantic-ui.com/dist/components/transition.css">
+  <link rel="stylesheet" type="text/css" href="https://semantic-ui.com/dist/components/table.css">
 
   <style type="text/css">
+    body {
+      background-color: rgb(27,28,29);
+    }
 
     .hidden.menu {
       display: none;
@@ -145,108 +149,66 @@ if(!isset($_SESSION['user'])) {
         <a class="toc item">
           <i class="sidebar icon"></i>
         </a>
-        <a class="active item">Home</a>
-        <a class="item">Work</a>
-        <a class="item">Company</a>
-        <a class="item">Careers</a>
+        <a class="active item">Game</a>
+        <a class="item">Rules</a>
+        <a class="item">Source</a>
         <div class="right item">
           <div class="ui inverted button">&euro; <?= $_SESSION['credit'] ?></div>
-          <a class="ui inverted button" href="logout.php">Logout</a>
+          <a class="ui inverted button" href="logout.php">Logout - <?= $_SESSION['user'] ?></a>
         </div>
       </div>
     </div>
 
-    <div class="ui text container">
-      <h1 class="ui inverted header">
-        Imagine-a-Company
-      </h1>
-      <h2>Do whatever you want when you want to.</h2>
-      <div class="ui huge primary button">Get Started <i class="right arrow icon"></i></div>
-    </div>
+    <br/>
 
-  </div>
-
-  <div class="ui vertical stripe segment">
-    <div class="ui middle aligned stackable grid container">
-      <div class="row">
-        <div class="eight wide column">
-          <h3 class="ui header">We Help Companies and Companions</h3>
-          <p>We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.</p>
-          <h3 class="ui header">We Make Bananas That Can Dance</h3>
-          <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
-        </div>
-        <div class="six wide right floated column">
-          <img src="assets/images/wireframe/white-image.png" class="ui large bordered rounded image">
-        </div>
-      </div>
-      <div class="row">
-        <div class="center aligned column">
-          <a class="ui huge button">Check Them Out</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="ui vertical stripe quote segment">
-    <div class="ui equal width stackable internally celled grid">
-      <div class="center aligned row">
-        <div class="column">
-          <h3>"What a Company"</h3>
-          <p>That is what they all say about us</p>
-        </div>
-        <div class="column">
-          <h3>"I shouldn't have gone with their competitor."</h3>
-          <p>
-            <img src="assets/images/avatar/nan.jpg" class="ui avatar image"> <b>Nan</b> Chief Fun Officer Acme Toys
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="ui vertical stripe segment">
-    <div class="ui text container">
-      <h3 class="ui header">Breaking The Grid, Grabs Your Attention</h3>
-      <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
-      <a class="ui large button">Read More</a>
-      <h4 class="ui horizontal header divider">
-        <a href="#">Case Studies</a>
-      </h4>
-      <h3 class="ui header">Did We Tell You About Our Bananas?</h3>
-      <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
-      <a class="ui large button">I'm Still Quite Interested</a>
-    </div>
-  </div>
-
-
-  <div class="ui inverted vertical footer segment">
-    <div class="ui container">
-      <div class="ui stackable inverted divided equal height stackable grid">
+    <div class="ui grid">
+      <div class="three column row">
         <div class="three wide column">
-          <h4 class="ui inverted header">About</h4>
-          <div class="ui inverted link list">
-            <a href="#" class="item">Sitemap</a>
-            <a href="#" class="item">Contact Us</a>
-            <a href="#" class="item">Religious Ceremonies</a>
-            <a href="#" class="item">Gazebo Plans</a>
+          <br/>
+          <h4>Leaderboard</h4>
+        </div>
+        <div class="ten wide column">
+          <div style="background-image: url('img/table.png'); width: 960px; height: 640px;">
+            <h4>Game 1</h4>
+            <div id="players" style="position: absolute; bottom: 230px; width: 95%;">
+              <table class="ui celled table" style="background: none; color: white; border: none; text-align: center;">
+                <tbody>
+                  <tr>
+                    <td>Player 4</td>
+                    <td>Player 3</td>
+                    <td>Player 2</td>
+                    <td>Player 1</td>
+                  </tr>
+                  <tr>
+                    <td>Score: 0</td>
+                    <td>Score: 0</td>
+                    <td>Score: 0</td>
+                    <td><b>Score: 0</b></td>
+                  </tr>
+                  <tr>
+                    <td>Not ready</td>
+                    <td>Not ready</td>
+                    <td>Not ready</td>
+                    <td>Not ready</td>
+                  </tr>
+                  <tr>
+                    <td><b>Bet: 0.50</b></td>
+                    <td><b>Bet: 0.50</b></td>
+                    <td><b>Bet: 0.50</b></td>
+                    <td><b>Bet: 0.50</b></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
         <div class="three wide column">
-          <h4 class="ui inverted header">Services</h4>
-          <div class="ui inverted link list">
-            <a href="#" class="item">Banana Pre-Order</a>
-            <a href="#" class="item">DNA FAQ</a>
-            <a href="#" class="item">How To Access</a>
-            <a href="#" class="item">Favorite X-Men</a>
-          </div>
-        </div>
-        <div class="seven wide column">
-          <h4 class="ui inverted header">Footer Header</h4>
-          <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+          <br/>
+          <h4>Bank statistics</h4>
         </div>
       </div>
     </div>
+
   </div>
 </div>
 
