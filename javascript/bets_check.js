e094503b76de063d8e140162a97720f02a4a8c54
Rik Aikman <rikaikman@hotmail.com>
2018-03-21 23:15:30 +0100

    no more error

--- javascript/bets_check.js
+++ javascript/bets_check.js
@@ -1,22 +1,83 @@
-$("#ready").click(function(){
+//$("#ready").click(function(){
+//
+//    var bet = $("#inputBet").val();
+//
+//    if(bet == 0){
+//        alert("Enter a bet to start");
+//    }else{
+//        $.ajax({url: "php/check_bets.php?bet=" + bet, success: function(result){
+//	  $('#serverMessage').html(result);
+//	}});
+//    
+////        $.ajax( {
+////            url: "/blackjack/php/check_bets.php",
+////            crossdomain: true,
+////            type: "POST",
+////            data: {playerBet : bet},						
+////            success: function(result) {
+////                    $('#playerMenu').append(result);
+////                    console.log()
+////            },
+////            error: function(err) {
+////                    console.log("THE ERROR: ",err)
+////            } 
+////        })  
+//    }
+//});
+//
 
-    var bet = $("#inputBet").val();
+// Variable to hold request
+var request;
 
-    if(bet == 0){
-        alert("Enter a bet to start");
-    }else{
-        $.ajax( {
-            url: "php/check_bets.php",
-            crossdomain: true,
-            type: "POST",
-            data: {playerBet : bet},						
-            success: function(result) {
-                    $('#playerMenu').append(result);
-                    console.log(result)
-            },
-            error: function(err) {
-                    console.log("THE ERROR: ",err)
-            } 
-        })  
+// Bind to the submit event of our form
+$("#ready").submit(function(event){
+
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
     }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.post('/check_bets.php', serializedData, function(response) {
+    console.log("Response: "+response);
+});
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
 });
--- php/check_bets.php
+++ php/check_bets.php
@@ -4,56 +4,57 @@
     require_once('../includes/credentials.php');
     
     $conn = createconnect($host,$dbusername,$dbpassword,$db_name);
-    
-    matchPlayerUser();
-    
-    function matchPlayerUser(){
-        global $player;
-    
-        $resultGames = $conn->query('SELECT * FROM blackjack_games;');
-        $players = [];
-
-        while($row = $resultGames->fetch_assoc()) {
-            $players[1] = $row['p1_id'];
-            $players[2] = $row['p2_id'];
-            $players[3] = $row['p3_id'];
-            $players[4] = $row['p4_id'];
-        }
-        
-        if($_SESSION['userid'] = $players[1]) {
-           return $player = 'p1';
-        }elseif($_SESSION['userid'] = $players[2]){
-            return $player = 'p2';
-        }elseif($_SESSION['userid'] = $players[3]){
-            return $player = 'p3';
-        }else{
-            return $player = 'p4';
-        }
-    }
-    
-   
-    $playerbet = $player.'_bet';
-    $playerReady = $player.'_ready';
-    
-    
-    
-    if(isset($_POST['bet'])) {
-
-        global $player;
-        global $playerReady;
-        global $playerbet;
-
-        $result = $conn->query('SELECT * FROM blackjack_games WHERE game_id = 1');
-        $bet = $_POST['bet'];
-        $ready = 1;
-            echo $bet;
-            echo $ready;
-         $_SESSION['credit'] = $_SESSION['credit'] - $bet;
-         
-         $updateBet = "UPDATE `blackjack_games` SET `".$playerbet."`='".$bet."' WHERE game_id = 1;";
-         $updateReady = "UPDATE `blackjack_games` SET `".$playerReady."`= '".$ready."' WHERE game_id = 1;";
-        
-         
-        $result = $conn->query('SELECT * FROM blackjack_games WHERE game_id = 1');
-
-    }
+    echo $_GET['bet'];
+//    matchPlayerUser();
+//    
+//    function matchPlayerUser(){
+//        global $player;
+//    
+//        $resultGames = $conn->query('SELECT * FROM blackjack_games;');
+//        $players = [];
+//
+//        while($row = $resultGames->fetch_assoc()) {
+//            $players[1] = $row['p1_id'];
+//            $players[2] = $row['p2_id'];
+//            $players[3] = $row['p3_id'];
+//            $players[4] = $row['p4_id'];
+//        }
+//        
+//        if($_SESSION['userid'] = $players[1]) {
+//            echo "<h1>WASSUPPP</h1>";
+//           return $player = 'p1';
+//        }elseif($_SESSION['userid'] = $players[2]){
+//            return $player = 'p2';
+//        }elseif($_SESSION['userid'] = $players[3]){
+//            return $player = 'p3';
+//        }else{
+//            return $player = 'p4';
+//        }
+//    }
+//    
+//   
+//    $playerbet = $player.'_bet';
+//    $playerReady = $player.'_ready';
+//    
+//    
+//    
+//    if(isset($_POST['bet'])) {
+//
+//        global $player;
+//        global $playerReady;
+//        global $playerbet;
+//
+//        $result = $conn->query('SELECT * FROM blackjack_games WHERE game_id = 1');
+//        $bet = $_POST['bet'];
+//        $ready = 1;
+//            echo $bet;
+//            echo $ready;
+//         $_SESSION['credit'] = $_SESSION['credit'] - $bet;
+//         
+//         $updateBet = "UPDATE `blackjack_games` SET `".$playerbet."`='".$bet."' WHERE game_id = 1;";
+//         $updateReady = "UPDATE `blackjack_games` SET `".$playerReady."`= '".$ready."' WHERE game_id = 1;";
+//        
+//         
+//        $result = $conn->query('SELECT * FROM blackjack_games WHERE game_id = 1');
+//
+//    }
