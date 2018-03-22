c02f2ccc8ea4f810fd6ec78245abf5a2a387e8d4
Rik Aikman <rikaikman@hotmail.com>
2018-03-22 22:18:11 +0100

    sending object back to cards_array.js from phpserver and into db

--- php/card_array.php
+++ php/card_array.php
@@ -8,6 +8,10 @@ cardValue();
 function cardValue(){
     $card1 = isset($_GET['card1']);
     $card2 = isset($_GET['card2']);
+    $inputValue = $card1+$card2;
+
+    $result= $conn->query("UPDATE `blackjack_games` SET `p".$_SESSION['seat']."_score`='".$inputValue."' WHERE game_id = 1;");
+    
     $cardArray = array(card1=>"$('#card".$_SESSION['seat']."_1').html(".$card1.")", card2=>"$('#card".$_SESSION['seat']."_2').html(".$card2.")");
          $cards = json_encode($cardArray);
     echo $cards;
