61e32b60618e15406c7ac8deb43bfddb723a6006
Rik Aikman <rikaikman@hotmail.com>
2018-03-22 22:43:23 +0100

    working circle with one card

--- javascript/cards_array.js
+++ javascript/cards_array.js
@@ -5,11 +5,11 @@
           var card1 = cardnumbers[Math.floor(Math.random() * cardnumbers.length)];
           var card2 = cardnumbers[Math.floor(Math.random() * cardnumbers.length)];
             console.log("card1 ", card1, "card2 ", card2);
-            $.ajax({url: "php/card_array.php?card1="+card1+"card2="+card2, success: function(result){
+            $.ajax({url: 'php/card_array.php?card1='+card1, success: function(result){
 
                     var cardObj = jQuery.parseJSON(result);
                     console.log("wooo", result );
-                    alert("CARD 1 ",cardObj.card1);
+                    $(`#card${cardObj.playerid}_2`).html(cardObj.card1);
                     console.log("CARD 2 ",cardObj.card2);
                     
             }});
--- php/card_array.php
+++ php/card_array.php
@@ -6,13 +6,13 @@ require_once('../credentials.php');
 $conn = createconnect($host,$dbusername,$dbpassword,$db_name);
 cardValue();
 function cardValue(){
-    $card1 = isset($_GET['card1']);
+    $card1 = $_GET['card1'];
     $card2 = isset($_GET['card2']);
     $inputValue = $card1+$card2;
 
 //   $result= $conn->query("UPDATE blackjack_games SET p".$_SESSION['seat']."_score=".$inputValue."");
     
-    $cardArray = array(card1=>"$('#card".$_SESSION['seat']."_1').html(".$card1.")", card2=>"$('#card".$_SESSION['seat']."_2').html(".$card2.")");
+    $cardArray = array(playerid=>$_SESSION['seat'], card1=>$card1, card2=>$card2);
          $cards = json_encode($cardArray);
     echo $cards;
     
