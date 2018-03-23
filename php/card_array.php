effe1dfb4eb166c63b870493aaa5825cd1dda5df
Rik Aikman <rikaikman@hotmail.com>
2018-03-23 13:07:22 +0100

    pushing cardvalue in db and showing it on frontend functional

--- php/card_array.php
+++ php/card_array.php
@@ -3,18 +3,40 @@
 session_start();
 
 require_once('../credentials.php');
-$conn = createconnect($host,$dbusername,$dbpassword,$db_name);
+
+
 cardValue();
+
 function cardValue(){
+    $conn = createconnect();
     $card1 = $_GET['card1'];
     $card2 = $_GET['card2'];
     $inputValue = $card1+$card2;
-//    echo $inputValue;
+    $seat = $_SESSION['seat'];
+    
+    $sql = "SELECT p".$seat."_score FROM blackjack_games WHERE game_id=1";
 
-   $result= $conn->query("UPDATE blackjack_games SET p".$_SESSION['seat']."_score=".$inputValue."");
-    
-    $cardArray = array(playerid=>$_SESSION['seat'], card1=>$card1, card2=>$card2, cardsValue=> $inputValue);
-         $cards = json_encode($cardArray);
-     echo $cards;
-    
+    $scoreCheck = $conn->query($sql);
+
+    while($row = $scoreCheck->fetch_assoc()) {
+	if($row["p".$seat."_score"] == 0){
+                $result = $conn->query("UPDATE blackjack_games SET p".$seat."_score = ".$inputValue." WHERE game_id = 1");
+
+            $cardArray = array('playerid'=>$seat, 'card1'=>$card1, 'card2'=>$card2, 'cardsValue'=> $inputValue);
+
+            $cards = json_encode($cardArray);
+            
+            echo $cards;
+     
+        }else{
+           $result= $conn->query("UPDATE blackjack_games SET p".$seat."_score=".$inputValue." WHERE game_id = 1");
+
+            $cardArray = array('playerid'=>$seat, 'card1'=>$card1, 'card2'=>$card2, 'cardsValue'=> $inputValue);
+            
+            $cards = json_encode($cardArray);
+            
+            echo $cards;
+        }
+
+    }
 }
