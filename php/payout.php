ad01a864611e293963dbaf1604089574aacf2c3c
Rik Aikman <rikaikman@hotmail.com>
2018-03-25 20:09:22 +0200

    comparing scores and delivering payout accordingly functional

--- php/payout.php
+++ php/payout.php
@@ -8,14 +8,14 @@ session_start();
     function PayOut(){
         
         $conn = createconnect();
-        $dealerScore = 1;
+        $dealerScore = $_SESSION['totalDealerCards'];;
         $bet = $_SESSION['bet'];
         $player = $_SESSION['seat'];
         $playerScore = $_SESSION['playerCards'];
         $userid = $_SESSION['userid'];
         $credit = $_SESSION['credit'];
         
-        if($playerScore <= 21 && $playerScore > $dealerScore){
+        if($playerScore <= 21 && $playerScore > $dealerScore || $dealerScore > 21){
             $newcredit = $credit + ($bet * 2);   
             $updateCredit = "UPDATE blackjack_users SET credit = ".$newcredit." WHERE id = ".$_SESSION['userid'];
             $updatecredit = $conn->query($updateCredit);
@@ -27,12 +27,21 @@ session_start();
             $selectBankBalance = "SELECT bank_balance FROM blackjack_games WHERE game_id = 1";
             
             $getBankBalance = $conn->query($selectBankBalance);
-            $value = mysql_fetch_object($getBankBalance);
-            $bankBalance = $value->bank_balance;
+            //get the value of bank_balance from the db:
+            while ($row = $getBankBalance->fetch_assoc()) {
+                 $bankBalance = $row["bank_balance"];
+            
             $newBankBalance =  $bankBalance + $bet;
 
             $updateBankBalance = "UPDATE blackjack_games SET bank_balance = ".$newBankBalance." WHERE game_id = 1";
-            echo "Player Has Lost, Bank Wins";
+            
+            $updatebankbalance = $conn->query($updateBankBalance);
+            
+            echo "Player Has Lost, Bank Wins ".$newBankBalance;
+            
+            }
+        }else{
+            echo "Player LOSES";
         }
         
 
